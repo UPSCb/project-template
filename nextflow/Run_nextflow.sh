4e3d491239245bb04ebd44b21112790f052a641f
EdoardoPiombo <edoardo.piombo@gmail.com>
2026-04-09 16:40:31 +0200

    Update Run_nextflow.sh

--- nextflow/Run_nextflow.sh
+++ nextflow/Run_nextflow.sh
@@ -9,7 +9,7 @@
 ml Nextflow/24.04.2
 
 
-nextflow run ./nf-core_rnaseq -r 3.23  \
+nextflow run ./nf-core_rnaseq -r 3.23.0  \
 -profile singularity,upscb -c nextflow/nextflow.config \
 -params-file nextflow/nf-params.json \
 -with-trace -with-report report_$(date "+%Y_%m_%d_%H_%M").html \
