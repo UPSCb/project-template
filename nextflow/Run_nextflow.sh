413941ae7cfabd817e7c87de5c4a8593bf72826c
EdoardoPiombo <edoardo.piombo@gmail.com>
2026-04-09 16:38:37 +0200

    Added resume option

---
+++ nextflow/Run_nextflow.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -A account
+#SBATCH -c 1
+#SBATCH -t 7-00:00:00
+#SBATCH -J nextflow_run
+#SBATCH -o nextflow.out
+#SBATCH -e nextflow.err
+
+ml Nextflow/24.04.2
+
+
+nextflow run ./nf-core_rnaseq -r 3.23  \
+-profile singularity,upscb -c nextflow/nextflow.config \
+-params-file nextflow/nf-params.json \
+-with-trace -with-report report_$(date "+%Y_%m_%d_%H_%M").html \
+-resume
